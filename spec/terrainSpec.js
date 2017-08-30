b876ac3fe2873f083ebca9b7adb9f61a18f34301
Toby Hamand <tobyhamand@gmail.com>
2017-08-30 11:46:07 +0100

    completed tests

--- spec/terrainSpec.js
+++ spec/terrainSpec.js
@@ -4,16 +4,37 @@ describe("Terrain", function() {
     terrain = new Terrain();
   });
 
-  describe(".tileArray", function(){
+  describe(".generate", function(){
     it("returns an array", function(){
-      expect(terrain.tileArray.constructor).toBe(Array);
+      expect(terrain.generate().constructor).toBe(Array);
     });
   });
 
-  describe(".generate", function(){
-    it("randomises the tileArray", function(){
-      terrain.generate();
-      expect(terrain.tileArray.constructor).toBe(Array);
+  describe(".generate2DArray", function(){
+    it("returns an array with the correct dimensions", function(){
+      var array = terrain.generate2DArray(2, 3);
+      expect(array.length).toBe(3);
+      expect(array[0].length).toBe(2);
+    });
+  });
+
+  describe(".generateWidthArray", function(){
+    it("returns an array of building widths that adds up to 20", function(){
+      var array = terrain.generateWidthArray();
+      var total = 0;
+      for(var i = 0; i < array.length; i ++) {
+        total += array[i];
+      }
+      expect(total).toBe(20);
+    });
+  });
+
+  describe(".generateHeightArray", function(){
+    it("generates an array of random numbers between 1 and 8", function(){
+      var array = terrain.generateHeightArray(5);
+      for(var i = 0; i < array.length; i ++) {
+        expect(array[i]).toBeLessThan(9);
+      }
     });
   });
 
--- src/terrain.js
+++ src/terrain.js
@@ -1,24 +1,20 @@
-
-
 (function(exports){
+
+  var terrainUnitWidth = 20;
+  var terrainUnitHeight = 12;
 
   function Terrain() {
     this.tileArray = [];
   }
 
-  var terrainUnitWidth = 40;
-  var terrainUnitHeight = 24;
-
   Terrain.prototype = {
     generate: function() {
-      var tileArray = get2DArray(terrainUnitWidth, terrainUnitHeight);
-      console.log(tileArray);
-      var widthArray = generateWidthArray();
+      var tileArray = this.generate2DArray(terrainUnitWidth, terrainUnitHeight);
+      var widthArray = this.generateWidthArray();
       var buildingCount = widthArray.length;
-      var heightArray = generateHeightArray(buildingCount);
+      var heightArray = this.generateHeightArray(buildingCount);
 
       var x = 0;
-      var y = tileArray.length - 1;
 
       for(var i = 0; i < buildingCount; i ++) {
         for(var width = widthArray[i]; width > 0; width --) {
@@ -30,6 +26,36 @@
           x += 1;
         }
       }
+      return tileArray;
+    },
+    generate2DArray: function(width, height) {
+      var array = [];
+
+      for(var y = height; y > 0; y--) {
+        var row = [];
+          for(var x = width; x > 0; x--) { row.push(0); }
+        array.push(row);
+      }
+      return array;
+    },
+    generateWidthArray: function() {
+      var widthArray = [];
+      var i = 0;
+      while(widthArray.reduce(add, 0) < terrainUnitWidth - 3) {
+        widthArray[i] = randombetween(1, 3);
+        i ++;
+      }
+      widthArray[i] = terrainUnitWidth - (widthArray.reduce(add, 0));
+      return widthArray;
+    },
+    generateHeightArray: function(length) {
+      var heightArray = [];
+      i = 0;
+      while(heightArray.length < length) {
+        heightArray[i] = randombetween(1, 8);
+        i ++;
+      }
+      return heightArray;
     }
   };
 
@@ -41,37 +67,5 @@
       return a + b;
   }
 
-  function generateWidthArray() {
-    var widthArray = [];
-    var i = 0;
-    while(widthArray.reduce(add, 0) < terrainUnitWidth - 3) {
-      widthArray[i] = randombetween(1, 3);
-      i ++;
-    }
-    widthArray[i] = terrainUnitWidth - (widthArray.reduce(add, 0));
-    return widthArray;
-  }
-
-  function generateHeightArray(length) {
-    var heightArray = [];
-    i = 0;
-    while(heightArray.length < length) {
-      heightArray[i] = randombetween(1, 8);
-      i ++;
-    }
-    return heightArray;
-  }
-
-  function get2DArray(width, height) {
-    var array = [];
-
-    for(var y = height; y > 0; y--) {
-      var row = [];
-        for(var x = width; x > 0; x--) { row.push(0); }
-      array.push(row);
-    }
-    return array;
-  }
-
-  exports.Terrain = terrain;
+  exports.Terrain = Terrain;
 })(this);
