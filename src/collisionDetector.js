42c2b10b7696beaa5a6bb3260076d21fc7c45c81
Ben Hughes <benjamin-hughes@users.noreply.github.com>
2017-08-30 13:45:53 +0100

    minor fix

--- src/collisionDetector.js
+++ src/collisionDetector.js
@@ -16,6 +16,7 @@
 //   }
 //   exports.CollisionDetector = CollisionDetector;
 // })(this);
+
 (function(exports) {
   CollisionDetector.prototype = {
     isHit: function(banana, tileMap) {
@@ -25,22 +26,22 @@
       var bottomTile = banana.y + 25 / 50;
       var anyCollision = false;
 
-      if(leftTile < 0) {
+      if (leftTile < 0) {
         leftTile = 0;
       }
-      if(rightTile > tileMap.width) {
+      if (rightTile > tileMap.width) {
         rightTile = tileMap.width;
       }
-      if(toptile < 0) {
+      if (toptile < 0) {
         topTile = 0;
       }
-      if(bottomTile > tileMap.height) {
+      if (bottomTile > tileMap.height) {
         bottomTile = tileMap.height;
       }
 
       anyCollision = false;
 
-      for(var i = leftTile; i <= rightTile; i++) {
+      for (var i = leftTile; i <= rightTile; i++) {
         for (var j = topTile; j <= bottomTile; i++) {
           var tile = tileMap.tileAt(i, j);
           if(tile.isWall) {
