4d46bc5b95896ae611a0e7b9926a57a33473fcaa
Alexander-Blair <zanderb@hotmail.co.uk>
2017-08-31 18:43:58 +0100

    Added tolerance of 10 pixels for more realistic collisions

--- spec/gorillaCollisionDetectorSpec.js
+++ spec/gorillaCollisionDetectorSpec.js
@@ -1,11 +1,11 @@
 'use strict';
 
 describe("Collision Detector", function() {
-  var collisionDetector, mockRectangle;
+  var collisionDetector, mockGorilla;
   var leftHitBanana, rightHitBanana, topHitBanana;
   var leftMissBanana, rightMissBanana, topMissBanana;
 
-  mockRectangle = {
+  mockGorilla = {
     xCoord: function() { return 100; },
     yCoord: function() { return 450; },
     width: function() { return 100; },
@@ -26,7 +26,7 @@ describe("Collision Detector", function() {
   }
 
   leftHitBanana = new MockBanana(100, 500);
-  rightHitBanana = new MockBanana(200, 500);
+  rightHitBanana = new MockBanana(180, 500);
   topHitBanana = new MockBanana(150, 450);
 
   leftMissBanana = new MockBanana(50, 500);
@@ -37,24 +37,24 @@ describe("Collision Detector", function() {
 
   describe("collision", function() {
     it("detects a hit to the left side", function() {
-      expect(collisionDetector.isHit(mockRectangle, leftHitBanana)).toBe(true);
+      expect(collisionDetector.isHit(mockGorilla, leftHitBanana)).toBe(true);
     });
     it("detects a hit to the right side", function() {
-      expect(collisionDetector.isHit(mockRectangle, rightHitBanana)).toBe(true);
+      expect(collisionDetector.isHit(mockGorilla, rightHitBanana)).toBe(true);
     });
     it("detects a hit to the top side", function() {
-      expect(collisionDetector.isHit(mockRectangle, topHitBanana)).toBe(true);
+      expect(collisionDetector.isHit(mockGorilla, topHitBanana)).toBe(true);
     });
   });
   describe("not a collision", function() {
     it("detects a hit to the left side", function() {
-      expect(collisionDetector.isHit(mockRectangle, leftMissBanana)).toBe(false);
+      expect(collisionDetector.isHit(mockGorilla, leftMissBanana)).toBe(false);
     });
     it("detects a hit to the right side", function() {
-      expect(collisionDetector.isHit(mockRectangle, rightMissBanana)).toBe(false);
+      expect(collisionDetector.isHit(mockGorilla, rightMissBanana)).toBe(false);
     });
     it("detects a hit to the top side", function() {
-      expect(collisionDetector.isHit(mockRectangle, topMissBanana)).toBe(false);
+      expect(collisionDetector.isHit(mockGorilla, topMissBanana)).toBe(false);
     });
   });
 });
--- src/gorillaCollisionDetector.js
+++ src/gorillaCollisionDetector.js
@@ -8,14 +8,14 @@
       var rightXCoord = hitBox.xCoord() + hitBox.width();
       var bottomYCoord = hitBox.yCoord() + hitBox.height();
 
-      if(banana.xCoord() > hitBox.xCoord() + hitBox.width() ||
-        banana.xCoord() + banana.width() < hitBox.xCoord() ||
-        banana.yCoord() + banana.height() < hitBox.yCoord() ||
-        banana.yCoord() > hitBox.yCoord() + hitBox.height()) {
+      if(banana.xCoord() > hitBox.xCoord() + hitBox.width() - 10 ||
+        banana.xCoord() + banana.width() < hitBox.xCoord() + 10 ||
+        banana.yCoord() + banana.height() < hitBox.yCoord() + 10 ||
+        banana.yCoord() > hitBox.yCoord() + hitBox.height() - 10) {
           return false;
       } else { return true; }
     }
   };
-  
+
   exports.GorillaCollisionDetector = GorillaCollisionDetector;
 })(this);
