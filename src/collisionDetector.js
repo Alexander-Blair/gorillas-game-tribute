c67e26ac456e315007cd6e247f6ff05e46c2b69a
Ben Hughes <benjamin-hughes@users.noreply.github.com>
2017-08-31 10:32:45 +0100

    refactored collisionDetector to only check bottom corners of hit box

--- src/collisionDetector.js
+++ src/collisionDetector.js
@@ -1,36 +1,48 @@
-
 (function(exports) {
   CollisionDetector.prototype = {
     isHit: function(banana, tileMap) {
-      var leftTile = banana.x - 25 / 50;
-      var rightTile = banana.x + 25 / 50;
-      var topTile = banana.y - 25 / 50;
-      var bottomTile = banana.y + 25 / 50;
-      var anyCollision = false;
+      // var leftTile     = banana.x - 25 / 50;
+      // var rightTile    = banana.x + 25 / 50;
+      // var topTile      = banana.y - 25 / 50;
+      // var bottomTile   = banana.y + 25 / 50;
+      var bLeft          = [banana.x, (banana.y + 50)];
+      var bRight         = [(banana.x + 50), (banana.y + 50)];
 
-      if (leftTile < 0) {
-        leftTile = 0;
-      }
-      if (rightTile > tileMap.width) {
-        rightTile = tileMap.width;
-      }
-      if (toptile < 0) {
-        topTile = 0;
-      }
-      if (bottomTile > tileMap.height) {
-        bottomTile = tileMap.height;
-      }
+      var leftConverted  = [Math.floor(bLeft[1]), Math.floor(bLeft[0])];
+      var rightConverted = [Math.floor(bRight[1]), Math.floor(bRight[0])];
+      var leftTile       = tileMap[leftConverted[0]][leftConverted[1]];
+      var rightTile      = tileMap[rightConverted[0]][rightConverted[1]];
 
-      anyCollision = false;
+      // var anyCollision = false;
+      //
+      // if (leftTile < 0) {
+      //   leftTile = 0;
+      // }
+      // if (rightTile > tileMap.width) {
+      //   rightTile = tileMap.width;
+      // }
+      // if (toptile < 0) {
+      //   topTile = 0;
+      // }
+      // if (bottomTile > tileMap.height) {
+      //   bottomTile = tileMap.height;
+      // }
+      //
+      // anyCollision = false;
+      //
+      // for (var i = leftTile; i <= rightTile; i++) {
+      //   for (var j = topTile; j <= bottomTile; i++) {
+      //     var xcoords = Math.floor(i / 50)
+      //     var yxoords = Math.floor(j / 50)
+      //     var tile = tileMap[y][x];
+      //     if(tile === 1) {
+      //       anyCollision = true;
+      //     }
+      //   }
+      // }
 
-      for (var i = leftTile; i <= rightTile; i++) {
-        for (var j = topTile; j <= bottomTile; i++) {
-          var tileCoords = [ Math.floor(i / 50), Math.floor(j / 50) ];
-          var tile = tileMap.tileAt(tileCoords[0], tileCoords[1]);
-          if(tile === 1) {
-            anyCollision = true;
-          }
-        }
+      if (leftTile === 1 || rightTile === 1 ) {
+        return true;
       }
     }
   }
