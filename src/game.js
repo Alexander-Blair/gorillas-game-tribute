b31bb643f645632228288f242f63dd4b55432534
Alexander Blair <zanderb@hotmail.co.uk>
2017-09-01 14:10:51 +0100

    Can choose best of (#22)

--- src/game.js
+++ src/game.js
@@ -5,6 +5,7 @@
     this.player1      = player1;
     this.player2      = player2;
     this._isPlayerOne = true
+    this._bestOf = 3;
   }
 
   Game.prototype.newRound = function() {
@@ -23,12 +24,28 @@
     return this.player1.score() + ':' + this.player2.score();
   }
 
+  Game.prototype.setBestOf = function(value) {
+    this._bestOf = value;
+  }
+
+  Game.prototype.winningScore = function() {
+    return Math.floor(this._bestOf / 2) + 1;
+  }
+
   Game.prototype.isGameOver = function() {
-    return this.player1.score() >= winningScore || this.player2.score() >= winningScore
+    return this.player1.score() >= this.winningScore() ||
+           this.player2.score() >= this.winningScore() ||
+           this.player1.score() + this.player2.score() >= this._bestOf;
   }
 
   Game.prototype.winner = function() {
-    return this.player1.score() >= winningScore ? this.player1 : this.player2;
+    if(this.player1.score() >= this.winningScore()) {
+      return this.player1;
+    }
+    if(this.player2.score() >= this.winningScore()) {
+      return this.player2;
+    }
+    return { name: function() { return 'Nobody!!!!'; } }
   }
 
   Game.prototype.isPlayerOne = function() {
